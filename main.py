b8d4342a29ad5105cbd27745c79960e0f64ba109
skiarxon <skiarxon@gmail.com>
2025-04-09 11:16:39 +0300

    new files based on lesson and changes for adding fps, close button

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 ##60 fps 
+
 
 if __name__ == "__main__":
     main()
