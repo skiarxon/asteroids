8d41457119de0f11c88dfa1abd08e0bf68aac6ed
skiarxon <skiarxon@gmail.com>
2025-04-11 14:52:29 +0300

    scoring added

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 import sys
 from shot import Shot
+import os
 
 def main():
     pygame.init()
+    pygame.font.init()
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    score = 0
+    score_increment = 10
 
     updatable = pygame.sprite.Group() # group for things that will be updated on screen
     drawable = pygame.sprite.Group() # group for things that are going to be drawn on screen
@@ -29,6 +33,7 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # moved this here or else the groups won't work properly
 
     while True:
+        font = pygame.font.Font(None, 36)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -52,9 +57,13 @@ def main():
                 if asteroid.collides_with(shot):
                    shot.kill()
                    asteroid.split()
+                   score += score_increment 
                    break  # Stop checking other shots for this asteroid     
 
         screen.fill("black")
+
+        score_text = font.render(f'Score: {score}', True, ("white"))
+        screen.blit(score_text, (10, 10))
 
         for obj in drawable:
             obj.draw(screen)
